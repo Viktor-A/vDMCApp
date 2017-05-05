2309c8621e84d555f50b6c8038948d6ef7669bbf
vittorio <vittorio>
2017-05-05 15:29:25 +0300

    Styling DMCA and compiling sass files

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>v DMCA App</title>
-
-	<link href="/css/app.css" rel="stylesheet">
+	<link href="/css/all.css" rel="stylesheet">
 </head>
 <body>
 	@include('partials.nav')
